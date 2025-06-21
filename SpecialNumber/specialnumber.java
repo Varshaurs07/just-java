760f4ef4cdd22dc7ebef371ed56e40767892cc6d
Varshaurs07 <varshaurs07@gmail.com>
2025-06-21 10:51:37 +0530

    Special Number

---
+++ SpecialNumber/specialnumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SpecialNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Special number between 10 and 99 are:");
+        for(int i=10; i<=99; i++){
+            int first = i/10;
+            int second = i%10;
+            int sum = first+second;
+            int product = first * second;
+
+            if(i== (sum + product)){
+                System.out.println(i);
+            }  
+        }
+    }
+
+}
